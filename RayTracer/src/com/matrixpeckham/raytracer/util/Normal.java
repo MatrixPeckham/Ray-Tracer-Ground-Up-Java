5b966ee1f47c74e4c259a9150f107f813a626991
MatrixPeckham <matrixpeckham@gmail.com>
2015-04-04 21:14:30 -0400

    Missed the normal class
    oops missed a class on last commit

--- RayTracer/src/com/matrixpeckham/raytracer/util/Normal.java
+++ RayTracer/src/com/matrixpeckham/raytracer/util/Normal.java
@@ -21,83 +21,125 @@ package com.matrixpeckham.raytracer.util;
  * @author William Matrix Peckham
  */
 public class Normal {
+
     public double x;
     public double y;
     public double z;
 
     // default ructor
     public Normal() {
-
+        this(0);
     }
 
     // ructor
     public Normal(double a) {
-
+        this(a, a, a);
+    }
+    public void setTo(double x, double y, double z){
+        this.x=x;
+        this.y=y;
+        this.z=z;
     }
 
     // ructor
     public Normal(double _x, double _y, double _z) {
-
+        x = _x;
+        y = _y;
+        z = _z;
     }
 
     // copy ructor
-    public Normal(
-            Normal n) {
-
+    public Normal(Normal n) {
+        this(n.x, n.y, n.z);
     }
 
     // ructs a normal from vector
-    public Normal(
-            Vector3D v) {
-
+    public Normal(Vector3D v) {
+        this(v.x, v.y, v.z);
     }
 
     // assignment operator
-    public Normal setTo(
-            Normal rhs) {
-
+    public Normal setTo(Normal rhs) {
+        x = rhs.x;
+        y = rhs.y;
+        z = rhs.z;
+        return this;
     }
 
     // assignment of a vector to a normal
-    public Normal setTo(
-            Vector3D rhs) {
-
+    public Normal setTo(Vector3D rhs) {
+        x = rhs.x;
+        y = rhs.y;
+        z = rhs.z;
+        return this;
     }
 
     // assignment of a point to a normal
     public Normal setTo(
             Point3D rhs) {
-
+        x = rhs.x;
+        y = rhs.y;
+        z = rhs.z;
+        return this;
     }
 
     // unary minus
     public Normal neg() {
-
+        return new Normal(-x, -y, -z);
     }
 
     // addition
     public Normal add(Normal n) {
-
+        return new Normal(x + n.x, y + n.y, z + n.z);
     }
 
     // compound addition
     public Normal addLocal(Normal n) {
-
+        x += n.x;
+        y += n.y;
+        z += n.z;
+        return this;
     }
 
     // dot product with a vector on the right
     public double dot(Vector3D v) {
-
+        return x * v.x + y * v.y + z * v.z;
     }
 
     // multiplication by a double on the right
     public Normal mul(double a) {
-
+        return new Normal(x * a, y * a, z * a);
     }
 
     // convert normal to a unit normal
     public void normalize() {
+        double len = Math.sqrt(x * x + y * y + z * z);
+        x /= len;
+        y /= len;
+        z /= len;
+    }
 
+    public static Normal mul(double a, Normal n) {
+        return new Normal(a * n.x, a * n.y, a * n.z);
+    }
+
+    public static Vector3D add(Vector3D v, Normal n) {
+        return new Vector3D(v.x + n.x, v.y + n.y, v.z + n.z);
+    }
+
+    public static Vector3D sub(Vector3D v, Normal n) {
+        return new Vector3D(v.x - n.x, v.y - n.y, v.z - n.z);
+    }
+
+    public static double dot(Vector3D v, Normal n) {
+        return v.x * n.x + v.y * n.y + v.z * n.z;
+    }
+
+    public static Normal mul(Matrix mat, Normal n) {
+        return new Normal(
+                mat.m[0][0] * n.x + mat.m[1][0] * n.y + mat.m[2][0] * n.z,
+                mat.m[0][1] * n.x + mat.m[1][1] * n.y + mat.m[2][1] * n.z,
+                mat.m[0][2] * n.x + mat.m[1][2] * n.y + mat.m[2][2] * n.z);
     }
 
 }
